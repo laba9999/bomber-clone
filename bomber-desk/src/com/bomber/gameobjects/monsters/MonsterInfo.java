ee2140b13d903542ce15f5e6f04c414e525271d2
martins.ajc@gmail.com <martins.ajc@gmail.com@0d58b223-b1b5-a743-caad-4bfc368ed673>
2011-12-08 18:43:11 +0000

    Implementação do tipo de monstros.

---
+++ bomber-desk/src/com/bomber/gameobjects/monsters/MonsterInfo.java
@@ -0,0 +1,58 @@
+package com.bomber.gameobjects.monsters;
+
+import java.util.HashMap;
+
+public class MonsterInfo
+{
+	public static HashMap<String, MonsterInfo> TYPES = null;
+	
+	public int mPointsValue;
+	public boolean mAbleToKill;
+	public boolean mAbleToFly;
+	
+	public MonsterInfo(int _pointsValue, boolean _ableToKill, boolean _ableToFly)
+	{
+		mPointsValue = _pointsValue;
+		mAbleToKill  = _ableToKill;
+		mAbleToFly = _ableToFly;
+	}
+	
+	public static MonsterInfo getFlagsFromType(String _type)
+	{
+		if(TYPES == null)
+		{
+			TYPES = new HashMap<String, MonsterInfo>();
+			
+			// Não matam e não voam
+			MonsterInfo tmpInfo = new MonsterInfo(100, false, false);
+			
+			TYPES.put("m_generic1", tmpInfo);
+			TYPES.put("m_generic2", tmpInfo);
+			TYPES.put("m_generic3", tmpInfo);
+			TYPES.put("m_generic4", tmpInfo);
+			TYPES.put("m_generic5", tmpInfo);
+			TYPES.put("m_generic6", tmpInfo);
+			TYPES.put("m_generic7", tmpInfo);
+			TYPES.put("m_generic8", tmpInfo);
+			TYPES.put("m_generic9", tmpInfo);
+			TYPES.put("m_generic10", tmpInfo);
+			
+			
+			// Matam mas não voam
+			tmpInfo = new MonsterInfo(250, true, false);
+			
+			TYPES.put("m_2", tmpInfo);
+			TYPES.put("m_3", tmpInfo);
+			TYPES.put("m_4", tmpInfo);
+			TYPES.put("m_5", tmpInfo);
+			
+			// Matam e voam
+			tmpInfo = new MonsterInfo(500, true, true);
+			
+			TYPES.put("m_1", tmpInfo);
+		}
+		
+		return TYPES.get(_type);
+	}
+
+}
