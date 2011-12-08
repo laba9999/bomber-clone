af39a52f44881c622f3a0f96ffbf0b8e038442fc
filipebrandao90@gmail.com <filipebrandao90@gmail.com@0d58b223-b1b5-a743-caad-4bfc368ed673>
2011-12-08 18:18:34 +0000

    Clean up às classes Assets e Level;

--- trunk/bomber-desk/src/com/bomber/world/Level.java
+++ trunk/bomber-desk/src/com/bomber/world/Level.java
@@ -14,8 +14,6 @@ import org.xml.sax.InputSource;
 import org.xml.sax.XMLReader;
 
 import com.bomber.common.Assets;
-import com.bomber.common.Factory;
-import com.bomber.common.ObjectFactory;
 import com.bomber.gameobjects.Player;
 import com.bomber.gameobjects.Tile;
 import com.bomber.gameobjects.monsters.Monster;
@@ -146,143 +144,56 @@ public class Level
 			short _positionY, GameWorld _world)
 	{
 		ImageTile temp = mImageTiles.get(_id);
-
+	
+		
 		//BOMBERMANS
 		if (temp.mImageFile.equals("spawn_p1"))
 		{
-			Assets.loadPlayer("white/b_white");
 			Player p = _world.mPlayers.getFreeObject();
 			p.mPosition.set(_positionX, _positionY);
-			p.mAnimations = Assets.mPlayers.get("white/b_white");
+			p.mAnimations = Assets.mPlayers.get("b_white");
 		}
 		else if (temp.mImageFile.equals("spawn_p2") && mNumberOfPlayers >= 2)
 		{
-			Assets.loadPlayer("red/b_red");
 			Player p = _world.mPlayers.getFreeObject();
 			p.mPosition.set(_positionX, _positionY);
-			p.mAnimations = Assets.mPlayers.get("red/b_red");
+			p.mAnimations = Assets.mPlayers.get("b_red");
 		}
 		else if (temp.mImageFile.equals("spawn_p3") && mNumberOfPlayers >= 3)
 		{
-			Assets.loadPlayer("blue/b_blue");
 			Player p = _world.mPlayers.getFreeObject();
 			p.mPosition.set(_positionX, _positionY);
-			p.mAnimations = Assets.mPlayers.get("blue/b_blue");
+			p.mAnimations = Assets.mPlayers.get("/b_blue");
 		}
-		else if (temp.mImageFile.equals("spawn_p4") && mNumberOfPlayers >= 4)
+		else if (temp.mImageFile.equals("spawn_p4") && mNumberOfPlayers == 4)
 		{
-			Assets.loadPlayer("green/b_green");
 			Player p = _world.mPlayers.getFreeObject();
 			p.mPosition.set(_positionX, _positionY);
-			p.mAnimations = Assets.mPlayers.get("green/b_green");
+			p.mAnimations = Assets.mPlayers.get("b_green");
 		}
-		//MONSTROS GENERICOS
-		else if (temp.mImageFile.contains("generic1"))
-		{
-			Assets.loadGenericMonster("generic/1/m_generic1");
-			Monster m = ObjectFactory.CreateMonster.create(Monster.GENERIC1, _world);
-			m.mAnimations = Assets.mMonsters.get("generic/1/m_generic1");
-			m.mPosition.set(_positionX, _positionY);
+		else
+		{//MONSTROS
+			
+			String[] splitted = temp.mImageFile.split("_");			
+			// ex: m_generic1_walk_0  tem monsterID m_generic1
+			String monsterID = splitted[0] + "_" + splitted[1];
+			
+			if(monsterID.contains("m_generic"))
+			{ //se é um monstro genérico
+				Assets.loadGenericMonster(monsterID);				
+			}
+			else
+			{
+				Assets.loadNormalMonster(monsterID);
+			}
+			
+			_world.spawnMonster(Monster.getTypeFromString(monsterID),_positionY,_positionX);
+			
 		}
-		else if (temp.mImageFile.contains("generic2"))
-		{
-			Assets.loadGenericMonster("generic/2/m_generic2");
-			Monster m = ObjectFactory.CreateMonster.create(Monster.GENERIC2, _world);
-			m.mAnimations = Assets.mMonsters.get("generic/2/m_generic2");
-			m.mPosition.set(_positionX, _positionY);
-		}
-		else if (temp.mImageFile.contains("generic3"))
-		{
-			Assets.loadGenericMonster("generic/3/m_generic3");
-			Monster m = ObjectFactory.CreateMonster.create(Monster.GENERIC3, _world);
-			m.mAnimations = Assets.mMonsters.get("generic/3/m_generic3");
-			m.mPosition.set(_positionX, _positionY);
-		}
-		else if (temp.mImageFile.contains("generic4"))
-		{
-			Assets.loadGenericMonster("generic/4/m_generic4");
-			Monster m = ObjectFactory.CreateMonster.create(Monster.GENERIC4, _world);
-			m.mAnimations = Assets.mMonsters.get("generic/4/m_generic4");
-			m.mPosition.set(_positionX, _positionY);
-		}
-		else if (temp.mImageFile.contains("generic5"))
-		{
-			Assets.loadGenericMonster("generic/5/m_generic5");
-			Monster m = ObjectFactory.CreateMonster.create(Monster.GENERIC5, _world);
-			m.mAnimations = Assets.mMonsters.get("generic/5/m_generic5");
-			m.mPosition.set(_positionX, _positionY);
-		}
-		else if (temp.mImageFile.contains("generic6"))
-		{
-			Assets.loadGenericMonster("generic/6/m_generic6");
-			Monster m = ObjectFactory.CreateMonster.create(Monster.GENERIC6, _world);
-			m.mAnimations = Assets.mMonsters.get("generic/6/m_generic6");
-			m.mPosition.set(_positionX, _positionY);
-		}
-		else if (temp.mImageFile.contains("generic7"))
-		{
-			Assets.loadGenericMonster("generic/7/m_generic7");
-			Monster m = ObjectFactory.CreateMonster.create(Monster.GENERIC7, _world);
-			m.mAnimations = Assets.mMonsters.get("generic/7/m_generic7");
-			m.mPosition.set(_positionX, _positionY);
-		}
-		else if (temp.mImageFile.contains("generic8"))
-		{
-			Assets.loadGenericMonster("generic/8/m_generic8");
-			Monster m = ObjectFactory.CreateMonster.create(Monster.GENERIC8, _world);
-			m.mAnimations = Assets.mMonsters.get("generic/8/m_generic8");
-			m.mPosition.set(_positionX, _positionY);
-		}
-		else if (temp.mImageFile.contains("generic9"))
-		{
-			Assets.loadGenericMonster("generic/9/m_generic9");
-			Monster m = ObjectFactory.CreateMonster.create(Monster.GENERIC9, _world);
-			m.mAnimations = Assets.mMonsters.get("generic/9/m_generic9");
-			m.mPosition.set(_positionX, _positionY);
-		}
-		else if (temp.mImageFile.contains("generic10"))
-		{
-			Assets.loadGenericMonster("generic/10/m_generic10");
-			Monster m = ObjectFactory.CreateMonster.create(Monster.GENERIC10, _world);
-			m.mAnimations = Assets.mMonsters.get("generic/10/m_generic10");
-			m.mPosition.set(_positionX, _positionY);
-		}
-		//MONSTROS NORMAIS
-		else if (temp.mImageFile.contains("m_1"))
-		{
-			Assets.loadGenericMonster("normal/1/m_1");
-			Monster m = ObjectFactory.CreateMonster.create(Monster.NORMAL1, _world);
-			m.mAnimations = Assets.mMonsters.get("normal/1/m_1");
-			m.mPosition.set(_positionX, _positionY);
-		}
-		else if (temp.mImageFile.contains("m_2"))
-		{
-			Assets.loadGenericMonster("normal/2/m_2");
-			Monster m = ObjectFactory.CreateMonster.create(Monster.NORMAL2, _world);
-			m.mAnimations = Assets.mMonsters.get("normal/2/m_2");
-			m.mPosition.set(_positionX, _positionY);
-		}
-		else if (temp.mImageFile.contains("m_3"))
-		{
-			Assets.loadGenericMonster("normal/3/m_3");
-			Monster m = ObjectFactory.CreateMonster.create(Monster.NORMAL3, _world);
-			m.mAnimations = Assets.mMonsters.get("normal/3/m_3");
-			m.mPosition.set(_positionX, _positionY);
-		}
-		else if (temp.mImageFile.contains("m_4"))
-		{
-			Assets.loadGenericMonster("normal/4/m_4");
-			Monster m = ObjectFactory.CreateMonster.create(Monster.NORMAL4, _world);
-			m.mAnimations = Assets.mMonsters.get("normal/4/m_4");
-			m.mPosition.set(_positionX, _positionY);
-		}
-		else if (temp.mImageFile.contains("m_5"))
-		{
-			Assets.loadGenericMonster("normal/5/m_5");
-			Monster m = ObjectFactory.CreateMonster.create(Monster.NORMAL5, _world);
-			m.mAnimations = Assets.mMonsters.get("normal/5/m_5");
-			m.mPosition.set(_positionX, _positionY);
-		}
+
+
 	}
+	
+	
 
 }
