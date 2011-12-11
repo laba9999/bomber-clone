9b691e5e9579636db563951032c60d89a4c047db
martins.ajc@gmail.com <martins.ajc@gmail.com@0d58b223-b1b5-a743-caad-4bfc368ed673>
2011-12-11 02:47:27 +0000

    Alteração da classe BonusSpeed de bonus temporário para bonus normal

--- trunk/bomber-desk/src/com/bomber/gameobjects/bonus/BonusSpeed.java
+++ trunk/bomber-desk/src/com/bomber/gameobjects/bonus/BonusSpeed.java
@@ -4,25 +4,17 @@ import com.badlogic.gdx.graphics.g2d.Animation;
 import com.bomber.common.Assets;
 import com.bomber.gameobjects.Player;
 
-public class BonusSpeed extends TemporaryBonus {
+public class BonusSpeed extends Bonus {
 
 	public BonusSpeed() {
-
-		super(2050);
 
 		Animation anim = Assets.mBonus.get("bonus_speed");
 		setCurrentAnimation(anim, (short) NUMBER_OF_ANIMATION_FRAMES, true, true);
 	}
 
 	@Override
-	public void onRemoveEffect()
+	public void applyEffect(Player _player)
 	{
-		mAffectedPlayer.mSpeed -= 1;
-	}
-
-	@Override
-	public void onApplyEffect()
-	{
-		mAffectedPlayer.mSpeed += 1;
+		_player.mSpeed += 0.25;
 	}
 }
