5dfd25e934f4c926a354b9b118ffc39db21899e4
martins.ajc@gmail.com <martins.ajc@gmail.com@0d58b223-b1b5-a743-caad-4bfc368ed673>
2011-12-12 00:50:24 +0000

    Adição de uma classe settings de debug, para activar e desactivar funcionalidades sem ter de comentar código.

---
+++ bomber-desk/src/com/bomber/DebugSettings.java
@@ -0,0 +1,16 @@
+package com.bomber;
+
+public class DebugSettings {
+	
+	// Players
+	public static final boolean PLAYER_DIE_WITH_EXPLOSIONS = false;
+	
+	// Monstros
+	public static final boolean MONSTERS_KILL_PLAYERS = false;
+	
+	// Mapa
+	public static final boolean MAP_LOAD_DESTROYABLE_TILES = false;
+	
+	// Mundo
+	public static final boolean WORLD_SPAWN_BONUS_RANDOMLY = true;
+}
