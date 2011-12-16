8c586a85d5f4786da328657abe13c0987bede1b1
filipebrandao90@gmail.com <filipebrandao90@gmail.com@0d58b223-b1b5-a743-caad-4bfc368ed673>
2011-12-16 02:40:13 +0000

    Gestão do HighScore no estado levelCompleted. com problemas : gravar novo high score!

--- trunk/bomber-desk/src/com/bomber/world/LevelInfo.java
+++ trunk/bomber-desk/src/com/bomber/world/LevelInfo.java
@@ -1,5 +1,12 @@
 package com.bomber.world;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+
 public class LevelInfo {
 	public String mCurrentLevelName;
 	public String mNextLevelName;
@@ -8,7 +15,7 @@ public class LevelInfo {
 	public short mSeconds;
 	public short mNumberBonus;
 	public short mBonusSeed;
-	public short mMaxPoints;
+	public int mHighScore;
 	/**
 	 * @param _levelInfo
 	 *            Posição 0: Nome do nivelseguinte
@@ -16,6 +23,7 @@ public class LevelInfo {
 	 *            Posição 2: Segundos
 	 *            Posição 3: Nº de bónus a spawnar
 	 *            Posição 4: Seed a usar no spawn dos bónus
+	 *            Posição 5: Pontuação Máxima actual
 	 */
 	public void set(String[] _levelInfo)
 	{
@@ -24,6 +32,33 @@ public class LevelInfo {
 		mSeconds = Short.valueOf(_levelInfo[2]);
 		mNumberBonus = Short.valueOf(_levelInfo[3]);
 		mBonusSeed = Short.valueOf(_levelInfo[4]);
-		mMaxPoints = Short.valueOf(_levelInfo[5]);
+		mHighScore = Short.valueOf(_levelInfo[5]);
+	}
+	
+	public void writeToFile()
+	{
+		//FileHandle fh = Gdx.files.internal("levels/" + mCurrentLevelName + "/info.txt");
+		FileHandle fh = Gdx.files.external("./assets/levels/" + mCurrentLevelName + "/info.txt");
+		
+		BufferedWriter bw = new BufferedWriter(fh.writer(false));
+		
+		try {
+			bw.write("#nome do próximo nível");
+			bw.write(mNextLevelName);
+			bw.write("#minutos");
+			bw.write(mMinutes);
+			bw.write("#segundos");
+			bw.write(mSeconds);
+			bw.write("#nº de bónus");
+			bw.write(mNumberBonus);
+			bw.write("#seed para os bónus");
+			bw.write(mBonusSeed);
+			bw.write("#highscore");			
+			bw.write(mHighScore);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
 	}
 }
