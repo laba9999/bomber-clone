d666b969b1c644c313954b926c2b7b7314cd7a0c
martins.ajc@gmail.com <martins.ajc@gmail.com@0d58b223-b1b5-a743-caad-4bfc368ed673>
2011-12-18 01:43:49 +0000

    Versão PVP.

--- bomber-android/src/com/amov/bomber/AndroidGame.java
+++ bomber-android/src/com/amov/bomber/AndroidGame.java
@@ -1,11 +1,19 @@
 package com.amov.bomber;
 
+import java.io.IOException;
+
 import android.os.Bundle;
 import android.view.WindowManager.LayoutParams;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
-import com.bomber.GameScreen;
-
+import com.bomber.Game;
+import com.bomber.GameClient;
+import com.bomber.remote.EventType;
+import com.bomber.remote.Message;
+import com.bomber.remote.MessageType;
+import com.bomber.remote.MessagesHandler;
+import com.bomber.remote.Protocols;
+import com.bomber.remote.RemoteConnections;
 
 public class AndroidGame extends AndroidApplication {
 
@@ -13,10 +21,29 @@ public class AndroidGame extends AndroidApplication {
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		// TODO Auto-generated method stub
-		
+
 		getWindow().addFlags(LayoutParams.FLAG_KEEP_SCREEN_ON);
 
 		super.onCreate(savedInstanceState);
-		initialize(new GameScreen(), false);
+		initialize(new Game(createConnections()), false);
 	}
+
+	private static RemoteConnections createConnections()
+	{
+		RemoteConnections connections = new RemoteConnections(true);
+
+		try
+		{
+			System.out.println("À espera de ligações...");
+			connections.acceptConnections(Protocols.TCP, 50005, (short) 1);
+			System.out.println("Server online!");
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		
+		return connections;
+	}
+	
 }
