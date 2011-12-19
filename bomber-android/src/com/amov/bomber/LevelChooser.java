22bd6a2545bd753786c0db355697f1c9d23c9e39
filipebrandao90@gmail.com <filipebrandao90@gmail.com@0d58b223-b1b5-a743-caad-4bfc368ed673>
2011-12-19 21:46:55 +0000

    Alterações na gallery e scrollview. Copiada tralha para a scrollbar com bolas....

--- bomber-android/src/com/amov/bomber/LevelChooser.java
+++ bomber-android/src/com/amov/bomber/LevelChooser.java
@@ -1,92 +1,41 @@
 package com.amov.bomber;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.res.TypedArray;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.BaseAdapter;
-import android.widget.Gallery;
-import android.widget.ImageView;
-import android.widget.Toast;
-
+import android.view.KeyEvent;
+import android.widget.HorizontalScrollView;
 
 /*
  * http://www.androidpeople.com/android-gallery-example
  * http://stackoverflow.com/questions/7797641/android-galleryview-recycling
  */
 public class LevelChooser extends Activity {
-	Gallery gallery;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.levels2);
+		setContentView(R.layout.levels);
 
-		gallery = (Gallery) findViewById(R.id.levelgallery);
-		gallery.setAdapter(new AddImgAdp(this));
+		HorizontalScrollView sv = (HorizontalScrollView) findViewById(R.id.levelsScrollView);
 
-		gallery.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView parent, View v, int position,
-					long id) {
-				// Displaying the position when the gallery item in clicked
-				Toast.makeText(LevelChooser.this, "Position=" + position,
-						Toast.LENGTH_SHORT).show();
-			}
-		});
+		sv.setBackgroundColor(Color.WHITE);
 
 	}
 
-	public class AddImgAdp extends BaseAdapter {
-		int mGalleryItemBackground;
-		private Context mContext;
-
-		// Adding images.
-		private Integer[] Imgid = { R.drawable.placeholder, R.drawable.placeholder,
-				R.drawable.placeholder, R.drawable.placeholder, R.drawable.placeholder, R.drawable.placeholder,
-				R.drawable.placeholder };
-
-		public AddImgAdp(Context c) {
-			mContext = c;
-			TypedArray typArray = obtainStyledAttributes(R.styleable.GalleryTheme);
-			mGalleryItemBackground = typArray.getResourceId(
-					R.styleable.GalleryTheme_android_galleryItemBackground, 0);
-			typArray.recycle();
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+			Intent resultIntent = new Intent();
+			setResult(Activity.RESULT_OK, resultIntent);
+			finish();
+			//desactiva animação na transição entre activities
+			overridePendingTransition(0, 0);
+			return true;
 		}
 
-		public int getCount() {
-			return Imgid.length;
-		}
-
-		public Object getItem(int position) {
-			return position;
-		}
-
-		public long getItemId(int position) {
-			return position;
-		}
-
-		
-		public View getView(int position, View convertView, ViewGroup parent) {
-
-		    if (!(convertView instanceof ImageView))
-		    {
-				ImageView iv = new ImageView(mContext);
-		        iv.setLayoutParams(new Gallery.LayoutParams(300, 300));
-		        iv.setScaleType(ImageView.ScaleType.FIT_XY);
-		        iv.setBackgroundResource(mGalleryItemBackground);
-		    	iv.setImageResource(Imgid[position]);
-		    	return iv;
-		    }
-
-			    return convertView;
-
-		}
-		
-		
+		return super.onKeyDown(keyCode, event);
 	}
 
 }
