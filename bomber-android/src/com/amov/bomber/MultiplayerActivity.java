804dd398b980b66a94ce8b1df0b35a851c6ea490
filipebrandao90@gmail.com <filipebrandao90@gmail.com@0d58b223-b1b5-a743-caad-4bfc368ed673>
2011-12-21 22:52:29 +0000

    Continuação da implementação das activities

--- bomber-android/src/com/amov/bomber/MultiplayerActivity.java
+++ bomber-android/src/com/amov/bomber/MultiplayerActivity.java
@@ -1,26 +1,100 @@
 package com.amov.bomber;
 
 import android.app.Activity;
-import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class MultiplayerActivity extends Activity
 {
+	
 	public static final int DIALOG_MULTIPLAYER = 0;
-
+	RadioButton mRadioWifi;
+	RadioButton mRadioClient;
+	TableRow mTableRowIpPort;
+	EditText mEditIp;
+	TextView mTextIp;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.multiplayer);
+		
+		mTableRowIpPort = (TableRow) findViewById(R.id.tableRowIpPort);
+		mEditIp = (EditText) findViewById(R.id.editIP);
+		mTextIp = (TextView) findViewById(R.id.textIp);
+		mRadioWifi = (RadioButton) findViewById(R.id.radioWifi);
+		mRadioClient = (RadioButton) findViewById(R.id.radioClient);
+	
+		//Listeners dos radio buttons
+		mRadioWifi.setOnCheckedChangeListener(new OnCheckedChangeListener()
+		{			
+			public void onCheckedChanged(CompoundButton _buttonView, boolean _isChecked)
+			{
+				if(_isChecked && mRadioClient.isChecked())
+				{
+					mTableRowIpPort.setVisibility(RadioButton.VISIBLE);
+					mEditIp.setVisibility(EditText.VISIBLE);
+					mTextIp.setVisibility(TextView.VISIBLE);
+				}
+				else if(_isChecked && !mRadioClient.isChecked())
+				{
+					mTableRowIpPort.setVisibility(RadioButton.VISIBLE);
+					mEditIp.setVisibility(EditText.GONE);
+					mTextIp.setVisibility(TextView.GONE);
+				}
+				else
+				{
+					mTableRowIpPort.setVisibility(RadioButton.GONE);
+				}
+			}
+		});
+		
+		mRadioClient.setOnCheckedChangeListener(new OnCheckedChangeListener()
+		{			
+			public void onCheckedChanged(CompoundButton _buttonView, boolean _isChecked)
+			{
+				if(_isChecked && mRadioWifi.isChecked())
+				{
+					mTableRowIpPort.setVisibility(RadioButton.VISIBLE);
+					mEditIp.setVisibility(EditText.VISIBLE);
+					mTextIp.setVisibility(TextView.VISIBLE);
+				}
+				else if(_isChecked && !mRadioWifi.isChecked())
+				{
+					mTableRowIpPort.setVisibility(RadioButton.GONE);
+				}
+				else if(!_isChecked && mRadioWifi.isChecked())
+				{
+					mEditIp.setVisibility(EditText.GONE);
+					mTextIp.setVisibility(TextView.GONE);
+				}
+				else
+				{
+					mTableRowIpPort.setVisibility(RadioButton.GONE);
+				}
+			}
+		});
+		
+
 
 	}
+	
+	public void onStartButton(View v)
+	{
+		Intent myIntent = new Intent(this, AndroidGame.class);
+		startActivityForResult(myIntent, 0);
+	}
+	
 	
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event)
