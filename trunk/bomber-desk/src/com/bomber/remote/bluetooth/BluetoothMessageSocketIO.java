7496a1372fcf88f4906c93352b04814b6d9a54ca
martins.ajc@gmail.com <martins.ajc@gmail.com@0d58b223-b1b5-a743-caad-4bfc368ed673>
2011-12-23 22:07:49 +0000

    A implementação do socket bluetooth que faltou no commit anterior.

---
+++ trunk/bomber-desk/src/com/bomber/remote/bluetooth/BluetoothMessageSocketIO.java
@@ -0,0 +1,72 @@
+package com.bomber.remote.bluetooth;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+
+import com.bomber.DebugSettings;
+import com.bomber.remote.MessageSocketIO;
+
+public class BluetoothMessageSocketIO extends MessageSocketIO {
+
+	private final BluetoothSocket mSocket;
+	private final InputStream mInStream;
+	private final OutputStream mOutStream;
+
+	public BluetoothMessageSocketIO(BluetoothSocket _socket) throws IOException {
+		mSocket = _socket;
+
+		mInStream = _socket.getInputStream();
+		mOutStream = _socket.getOutputStream();
+	}
+
+	public BluetoothMessageSocketIO(String _macAddress) throws IOException {
+		BluetoothDevice btDevice = DebugSettings.BLUETOOTH_ADAPTER.getRemoteDevice(_macAddress);
+		mSocket = btDevice.createRfcommSocketToServiceRecord(DebugSettings.APP_UUID);
+
+		mInStream = mSocket.getInputStream();
+		mOutStream = mSocket.getOutputStream();
+	}
+
+	@Override
+	protected void onClose()
+	{
+		try
+		{
+			mSocket.close();
+		} catch (IOException e)
+		{
+		}
+	}
+
+	@Override
+	protected boolean onSendMessage()
+	{
+		try
+		{
+			mOutStream.write(mSendBytes);
+			return true;
+		} catch (IOException e)
+		{
+			return false;
+		}
+	}
+
+	@Override
+	protected boolean onRecvMessage()
+	{
+		try
+		{
+			mInStream.read(mRecvBytes);
+			return true;
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+}
