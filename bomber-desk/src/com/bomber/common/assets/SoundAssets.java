b0157a305fa7083a1848cb7f3b4c3aa1c8cc0196
martins.ajc@gmail.com <martins.ajc@gmail.com@0d58b223-b1b5-a743-caad-4bfc368ed673>
2012-01-07 17:05:53 +0000

    Forrado a try/catch's

--- bomber-desk/src/com/bomber/common/assets/SoundAssets.java
+++ bomber-desk/src/com/bomber/common/assets/SoundAssets.java
@@ -20,6 +20,7 @@ public class SoundAssets {
 
 	public static boolean mIsSoundActive = false;
 	public static boolean mIsloaded = false;
+	public static boolean mFailedLoading = false;
 
 	private static Music mMusicPlaying = null;
 
@@ -31,33 +32,47 @@ public class SoundAssets {
 
 	public static void load()
 	{
+		mFailedLoading = false;
 		mMusicPlaying = null;
 
 		mMusics = new HashMap<String, Music>(10);
+		try
+		{
+			mMusics.put("intro", Gdx.audio.newMusic(Gdx.files.internal("sfx/m_011.ogg")));
+			mMusics.put("level1", Gdx.audio.newMusic(Gdx.files.internal("sfx/m_009.ogg")));
+			mMusics.put("level2", Gdx.audio.newMusic(Gdx.files.internal("sfx/m_003.ogg")));
+			mMusics.put("level3", Gdx.audio.newMusic(Gdx.files.internal("sfx/m_004.ogg")));
+			mMusics.put("level4", Gdx.audio.newMusic(Gdx.files.internal("sfx/m_005.ogg")));
+			mMusics.put("level5", Gdx.audio.newMusic(Gdx.files.internal("sfx/m_006.ogg")));
+			mMusics.put("level6", Gdx.audio.newMusic(Gdx.files.internal("sfx/m_007.ogg")));
+			mMusics.put("level7", Gdx.audio.newMusic(Gdx.files.internal("sfx/m_008.ogg")));
+			mMusics.put("level8", Gdx.audio.newMusic(Gdx.files.internal("sfx/m_002.ogg")));
+			mMusics.put("timeEnding", Gdx.audio.newMusic(Gdx.files.internal("sfx/time_ending.ogg")));
 
-		mMusics.put("intro", Gdx.audio.newMusic(Gdx.files.internal("sfx/m_011.ogg")));
-		mMusics.put("level1", Gdx.audio.newMusic(Gdx.files.internal("sfx/m_009.ogg")));
-		mMusics.put("level2", Gdx.audio.newMusic(Gdx.files.internal("sfx/m_003.ogg")));
-		mMusics.put("level3", Gdx.audio.newMusic(Gdx.files.internal("sfx/m_004.ogg")));
-		mMusics.put("level4", Gdx.audio.newMusic(Gdx.files.internal("sfx/m_005.ogg")));
-		mMusics.put("level5", Gdx.audio.newMusic(Gdx.files.internal("sfx/m_006.ogg")));
-		mMusics.put("level6", Gdx.audio.newMusic(Gdx.files.internal("sfx/m_007.ogg")));
-		mMusics.put("level7", Gdx.audio.newMusic(Gdx.files.internal("sfx/m_008.ogg")));
-		mMusics.put("level8", Gdx.audio.newMusic(Gdx.files.internal("sfx/m_002.ogg")));
-		mMusics.put("timeEnding", Gdx.audio.newMusic(Gdx.files.internal("sfx/time_ending.ogg")));
+			mMusics.put("levelISEC", Gdx.audio.newMusic(Gdx.files.internal("sfx/m_010.ogg")));
 
-		mMusics.put("levelISEC", Gdx.audio.newMusic(Gdx.files.internal("sfx/m_010.ogg")));
+			mSounds = new HashMap<String, Sound>();
+			mSounds.put("explosion", Gdx.audio.newSound(Gdx.files.internal("sfx/s_5.ogg")));
+			mSounds.put("bling", Gdx.audio.newSound(Gdx.files.internal("sfx/ring.ogg")));
+			mSounds.put("die", Gdx.audio.newSound(Gdx.files.internal("sfx/s_1.ogg")));
+		} catch (Exception e)
+		{
+			mMusicPlaying = null;
+			mFailedLoading = true;
 
-		mSounds = new HashMap<String, Sound>();
-		mSounds.put("explosion", Gdx.audio.newSound(Gdx.files.internal("sfx/s_5.ogg")));
-		mSounds.put("bling", Gdx.audio.newSound(Gdx.files.internal("sfx/ring.ogg")));
-		mSounds.put("die", Gdx.audio.newSound(Gdx.files.internal("sfx/s_1.ogg")));
+			SharedPreferences.Editor edit = Settings.GAME_PREFS.edit();
+			edit.putBoolean("soundEnabled", mIsSoundActive);
+			edit.commit();
+		}
 
 		mIsloaded = true;
 	}
 
 	public static void playMusic(String _music, boolean _looping, float _volume)
 	{
+		if (mFailedLoading)
+			return;
+
 		if (mPaused)
 		{
 			resume();
@@ -72,18 +87,28 @@ public class SoundAssets {
 		if (!mIsSoundActive)
 			return;
 
-		if (mMusicPlaying != null)
-			mMusicPlaying.stop();
+		try
+		{
+			if (mMusicPlaying != null)
+				mMusicPlaying.stop();
 
-		mMusicPlaying = mMusics.get(_music);
-		mMusicPlaying.setVolume(_volume);
-		mMusicPlaying.setLooping(_looping);
-		mMusicPlaying.play();
+			mMusicPlaying = mMusics.get(_music);
+			mMusicPlaying.setVolume(_volume);
+			mMusicPlaying.setLooping(_looping);
+			mMusicPlaying.play();
+		} catch (Exception e)
+		{
+			mMusicPlaying = null;
+
+			SharedPreferences.Editor edit = Settings.GAME_PREFS.edit();
+			edit.putBoolean("soundEnabled", mIsSoundActive);
+			edit.commit();
+		}
 	}
 
 	public static void playSound(String _sound)
 	{
-		if (!mIsSoundActive)
+		if (!mIsSoundActive || mFailedLoading)
 			return;
 
 		mSounds.get(_sound).play();
@@ -91,7 +116,7 @@ public class SoundAssets {
 
 	public static void resume()
 	{
-		if (mMusicPlaying == null || !mIsSoundActive)
+		if (mMusicPlaying == null || !mIsSoundActive || mFailedLoading)
 			return;
 
 		mMusicPlaying.play();
@@ -100,7 +125,7 @@ public class SoundAssets {
 
 	public static void pause()
 	{
-		if (mMusicPlaying == null)
+		if (mMusicPlaying == null || mFailedLoading)
 			return;
 
 		mPaused = true;
@@ -111,7 +136,7 @@ public class SoundAssets {
 	{
 		mPaused = false;
 		mLastMusicPlaying = null;
-		if (mMusicPlaying == null)
+		if (mMusicPlaying == null || mFailedLoading)
 			return;
 
 		mMusicPlaying.stop();
@@ -120,7 +145,9 @@ public class SoundAssets {
 
 	public static void toggle()
 	{
-
+		if (mFailedLoading)
+			return;
+		
 		mIsSoundActive = !mIsSoundActive;
 
 		SharedPreferences.Editor edit = Settings.GAME_PREFS.edit();
